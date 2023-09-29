e373ec90213f56ef1e21ffa0c79fe0f041cacbd1
Kaycal <riley.robertson5@gmail.com>
2023-09-29 18:59:59 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hack The Tunnels 2023
+Hackathon at CU focusing on web development and javascript!
---
+++ day1/helloworld.js
@@ -0,0 +1,4 @@
+// call a function on each element of an array using forEach
+let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+colors.forEach((color) => console.log("dying hair:", color));
+colors.map((color) => color.toUpperCase()).forEach((color) => console.log("dying hair:", color));
